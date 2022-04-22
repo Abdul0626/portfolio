face3878daea9292587aa0efb674c7b82a744b51
MacUer <macuer@macuers-mbp.home>
2022-04-22 02:24:01 -0400

    some message

---
+++ jsLessons/index.js
@@ -0,0 +1,25 @@
+var theDate = new Date();
+console.log(theDate);
+
+var pastDate = new Date(1545, 2, 9);
+var futureDate = new Date(2023, 0, 1);
+
+
+var birthday = new Date(1545,2,9,10,35,45);
+
+
+if(birthday == birthday2){
+    console.log('Birthday buddies');
+} else {
+    console.log('Birthdays are not equal');
+}
+
+/*
+console.log(birthday.getFullYear());
+console.log(birthday.getDate());
+console.log(birthday.getMonth())
+console.log(birthday.getDay());
+console.log(birthday.getHours());
+console.log(birthday.getTime());
+*/
+
